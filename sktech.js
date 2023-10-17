64d0b107b544d0e1ba0f6ecb946eb5969bd804f3
01AnaCarolina <143550019+01AnaCarolina@users.noreply.github.com>
2023-10-17 16:50:21 -0300

    adicionando arq js e css

---
+++ sktech.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+background("white")
+rect (0,0,100,150)
+}
+function setup() {
+
+createCanvas(600, 600);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("blue");
+
+fill("red");
+
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 35);
+
+}
+
+}
+
